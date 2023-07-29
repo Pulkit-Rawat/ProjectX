a3b761bb2334e0b57d5aaf58a7a6de78e0323ef5
Pulkit-Rawat <pulkitrawat.27@gmail.com>
2023-07-29 17:50:20 +0530

    Env setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+      <div>
+        <h3 className='text-center mt-5'>Welcome to Project-X</h3>
+      </div>
+    );
+}
+
+export default App;
